78043d78196cf04b63235fbe5ff14cabb6587d5b
dharryc <dharrisoncook@gmail.com>
2026-01-15 20:19:24 -0700

    created the project

---
+++ Hearts/Hearts/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Hearts
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Hearts/Hearts/Pages/Scores.xaml.cs
@@ -0,0 +1,17 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+namespace Hearts.Pages;
+
+
+public partial class Scores : ObservableObject
+{
+	public Scores()
+	{
+		InitializeComponent();
+	}
+}
+public partial class Player
+{
+
+}
